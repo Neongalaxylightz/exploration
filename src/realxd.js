dd19f379254a72387eb4e87d11fd8cc01324ddd5
Neongalaxylightz <85358722+Neongalaxylightz@users.noreply.github.com>
2021-06-04 15:44:00 -0400

    updates
    Y BUGZ

--- src/realxd.js
+++ src/realxd.js
@@ -2,4 +2,4 @@ async function myip() {
 return await (await fetch("https://ifconfig.me/ip")).text();
 }
 var iplabelVar = iplbl;
-iplabelVar.innerHTML = "Hello Person, Ur Ip For Coming Here Iz: "+myip()+" :)";
+iplabelVar.innerHTML = "Hello Person, Ur Ip For Coming Here Iz: "+await myip()+" :)";
