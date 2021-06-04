bb946448026ec81857be16a18e8f250746d76ffd
Neongalaxylightz <85358722+Neongalaxylightz@users.noreply.github.com>
2021-06-04 15:34:06 -0400

    Update realxd.js

--- src/realxd.js
+++ src/realxd.js
@@ -1,5 +1,5 @@
 async function myip() {
-return await (await fetch("https://ipconfig.me/ip"));
+return await (await fetch("https://ifconfig.me/ip")).txt;
 }
-var iplabelVar = document.getElementById("iplbl");
+var iplabelVar = document.
 iplabelVar.innerHTML = "Hello Person, Ur Ip For Coming Here Iz: "+myip()+" :)";
